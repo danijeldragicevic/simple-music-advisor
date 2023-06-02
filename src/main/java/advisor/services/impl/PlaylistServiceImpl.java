565308715b34a8e6d523f500fe2205035151f220
Danijel Dragicevic <077226@canda.com>
2023-06-02 09:41:06 +0200

    Refactor PlaylistServiceImpl.

--- src/main/java/advisor/services/impl/PlaylistServiceImpl.java
+++ src/main/java/advisor/services/impl/PlaylistServiceImpl.java
@@ -21,26 +21,21 @@ public class PlaylistServiceImpl implements IPlaylistService {
     
     @Override
     public List<Playlist> getAll(String accessToken) {
-        HttpRequest request = authRepository.createAuthorizationReq(accessToken, ExternalApiConfig.API_SERVER_PATH + ExternalApiConfig.FEATURED_PLAYLISTS_PATH);
-        String response = playlistRepository.getAll(request);
-
-        JsonObject jsonResponse = JsonParser.parseString(response).getAsJsonObject();
-        JsonObject jsonPlaylists = jsonResponse.getAsJsonObject("playlists");
+        HttpRequest request = authRepository.createAuthorizationReq(
+                accessToken, ExternalApiConfig.API_SERVER_PATH + ExternalApiConfig.FEATURED_PLAYLISTS_PATH);
         
-        List<Playlist> playlists = new ArrayList<>();
-        for (JsonElement item: jsonPlaylists.getAsJsonArray("items")) {
-            Playlist playlist = new Playlist();
-            playlist.setName(item.getAsJsonObject().get("name").toString().replaceAll("\"", ""));
-            playlist.setExternalUrl(item.getAsJsonObject().get("external_urls").getAsJsonObject().get("spotify").toString().replaceAll("\"", ""));
-            playlists.add(playlist);
-        }
-        
-        return playlists;
+        return getPlaylists(request);
     }
 
     @Override
     public List<Playlist> getByCategoryName(String accessToken, String cName) {
-        HttpRequest request = authRepository.createAuthorizationReq(accessToken, ExternalApiConfig.API_SERVER_PATH + ExternalApiConfig.CATEGORIES_PATH + "/" + cName + "/playlists");
+        HttpRequest request = authRepository.createAuthorizationReq(
+                accessToken, ExternalApiConfig.API_SERVER_PATH + ExternalApiConfig.CATEGORIES_PATH + "/" + cName + "/playlists");
+        
+        return getPlaylists(request);
+    }
+
+    private List<Playlist> getPlaylists(HttpRequest request) {
         String response = playlistRepository.getAll(request);
 
         JsonObject jsonResponse = JsonParser.parseString(response).getAsJsonObject();
